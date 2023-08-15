9951709b9489758b1207efddde5a3815d1ed65e8
gdecampos <gdecamposjst@gmail.com>
2023-08-15 16:02:06 -0300

    adjustments in the game settings

--- flappy_my.py
+++ flappy_my.py
@@ -3,7 +3,7 @@ import os
 import random
 
 # configuração da tela de jogo
-screen_width = 500
+screen_width = 550
 screen_height = 800
 
 # pygame.transform.scale2x() aumenta a imagem por 2x // pygame.image.load('filename') carrega imagens // os.path.join('foldername', 'filename') trás im,agens de outra pasta
@@ -20,13 +20,14 @@ bird_poses = [
 pygame.font.init()
 score_font = pygame.font.SysFont('georgia', 50) #pygame.font.SysFont('fontname', size) utiliza uma lista prórpia de fontes para fontes personalizadas salvar o arquivo da fonte como .ttf e utilizar o comando pygame.font.Font('Filepath", size)
 
-class bird:
+class Bird:
     imgs = bird_poses
     
     # animações da rotação
     max_rotation = 25
     rotation_speed = 20
     animation_time = 5
+    y = 0
     
     # definindo parametros iniciais do passaro
     def __init__(self, x, y):
@@ -40,23 +41,23 @@ class bird:
         self.image = self.imgs[0]
         
     def jump(self):
-        self.speed = -10.5
+        self.speed = -8 
         self.time = 0
         self.heigt = self.y
         
     def move(self):
-        # calculo do deslocamento
+        # calculo do deslocamento de queda
         self.time += 1
-        movement = 1.5 * (self.time**2) + self.speed * self.time # S=Si+V*T+(AT²)/2  // fórmula do sorvetão
+        movement = 1.3 * (self.time**2) + self.speed * self.time # S=Si+V*T+(AT²)/2  // fórmula do sorvetão
         
-        # restringir o deslocamento
-        if movement > 16:
-            movement = 16
+        # restringir velocidade de queda
+        if movement > 15:
+            movement = 15
         # dá uma vantagem no pulo devido a velocidade de queda
         elif movement < 0:
-            movement -=2
+            movement -= 2
         
-        # movimentando o passaro
+        # movimentando o passaro para baixo
         self.y += movement
         
         # angulo do passaro
@@ -99,11 +100,11 @@ class bird:
         screen.blit(rotate_image, box.topleft) # .blit serve para mostrar imagem na tela do pygame
 
     def get_mask(self):
-        pygame.mask.from_surface(self.image) # pega a mascara do passaro
+        return pygame.mask.from_surface(self.image) # pega a mascara do passaro
 
-class pipe:
+class Pipe:
     distance = 200 # Define a distancia entre o cano superior e inferior
-    pipe_speed = 5 # Define velocidade do cano
+    speed = 5 # Define velocidade do cano
     
     def __init__(self, x):
         self.x = x
@@ -120,8 +121,8 @@ class pipe:
         self.upper_position = self.height_pipe - self.upper_pipe.get_height() # define o cano para cima a partir do ponto definido no self.height_pipe
         self.bottom_position = self.height_pipe + self.distance # define o cano para baixo a partir do ponto definido no self.height_pipe mais a distancia entre os canos definida na variavel "distance"
         
-    def move_pipe(self):
-        self.x -= self.pipe_speed # Movimenta os canos da direita para esquerda (movimento negativo)
+    def move(self):
+        self.x -= self.speed # Movimenta os canos da direita para esquerda (movimento negativo)
         
     def sketch(self, screen):
         screen.blit(self.upper_pipe, (self.x, self.upper_position))
@@ -145,7 +146,7 @@ class pipe:
         else:
             return False
 
-class ground:
+class Ground:
     speed = 5
     ground_width = ground_skin.get_width()
     image = ground_skin
@@ -162,40 +163,44 @@ class ground:
         
         # Caso um dos chãos chegue ao fim da tela o envia para trás do próximo chão (cria um efeito carrocel)
         if self.x1 + self.ground_width < 0:
-            self.x1 = self.x1 + self.ground_width
+            self.x1 = self.x2 + self.ground_width
             
         if self.x2 + self.ground_width < 0:
-            self.x2 = self.x2 + self.ground_width
+            self.x2 = self.x1 + self.ground_width
             
     def sketch(self, screen):
         screen.blit(self.image, (self.x1, self.y))
         screen.blit(self.image, (self.x2, self.y))
         
-def screen_sketch(screen, birds, pipes, ground, score):
+def screen_sketch(screen, bird, pipes, ground, score):
     screen.blit(background_skin, (0, 0))
     # As funções estão no plural para serem usadas em diversos objetos na tela ao mesmo tempo
-    for bird in birds:
-        bird.sketch(screen)
+    bird.sketch(screen)
         
     for pipe in pipes:
         pipe.sketch(screen)
         
-    text = score_font.render(f"Score: {score}", 1, (50,50,50)) 
-    screen.blit(text, (screen_width - 10 - text.get_width(), 10))
+    text = score_font.render(f"{score} and {Pipe.speed}", 1, (50,50,50)) 
+    screen.blit(text, (screen_width/2  - text.get_width()/2, 10))
     ground.sketch(screen)
     pygame.display.update()
     
 def main():
-    birds = [bird(230, 350)]
-    ground = ground(730)
-    pipes = [pipe(700)]
+    bird = Bird(200, 350)
+    ground = Ground(730)
+    pipes = [Pipe(700)]
     screen = pygame.display.set_mode((screen_width, screen_height))
-    score = 0
+    score = 9
     clock = pygame.time.Clock()
-    
+    lost = False
     runing = True
+    
     while runing:
-        clock.tick(40) # .tick define o frame rate
+        clock.tick(30) # .tick define o frame rate 
+        # Movimenta os objetos em tela
+        if not lost:
+            bird.move()
+        ground.move()
         
         # Interações com o usuário
         for event in pygame.event.get(): # Verifica se o botão de fechar foi clicado
@@ -206,35 +211,43 @@ def main():
             
             if event.type == pygame.KEYDOWN: # Acionado quando uma tecla for pressionada
                 if event.key == pygame.K_SPACE: # Verifica se a tecla pressionada foi o espaço
-                    for bird in birds:
-                        bird.jump()
-        
-        # Movimenta os objetos em tela
-        for bird in birds:
-            bird.move()
-        ground.move()
+                    bird.jump()
         
         pipe_add = False
         pipe_remove = []
-        for pipe in pipes:
-            for i, bird in enumerate(birds): 
-                if pipe.collide(bird):  # Verifica se houve colisão entre o cano e o passaro
-                    birds.pop(i)    # Remove o passaro que colidiu da lista de passaros
-                if not pipe.trespass and bird.x > pipe.x: # Analisa se o passaro passou do cano
-                    pipe.trespass = True
-                    pipe_add = True 
-            pipe.move()
+        for pipe in pipes:           
+            pipe.move()  
+            
+            if pipe.collision(bird):  # Verifica se houve colisão entre o cano e o passaro
+                Pipe.speed = 0
+                Ground.speed = 0
+                Bird.animation_time = 0
+                lost = True
+                
+            if not pipe.trespass and bird.x > pipe.x: # Analisa se o passaro passou do cano
+                pipe.trespass = True
+                pipe_add = True 
+            
             if pipe.x + pipe.upper_pipe.get_width() < 0: # Verifica se o cano já saiu por completo da tela
                 pipe_remove.append(pipe) # Adiciona o cano em uma lista para remoção
                 
-        if pipe_add:
+        if pipe_add: # Função para adicionar mais canos
+            pipes.append(Pipe(650))     # Adiciona mais uma dupla de canos ao fim da tela     
+            if score % 10 == 0 and score != 0: # Acada 10 pontos aumenta a velocidade em 1
+                Pipe.speed += 1 
+                Ground.speed += 1
             score += 1
-            pipe.append(pipe(600))
+            
         for pipe in pipe_remove:
             pipes.remove(pipe)
         
-        for i, bird in enumerate(birds):
-            if (bird.y + bird.image.get_height()) > ground.y or bird.y < 0:
-                birds.pop(i)
-            
-        screen_sketch(screen, birds, pipes, ground, score)
+        if (bird.y + bird.image.get_height()) > ground.y or bird.y < 0:
+            Pipe.speed = 0
+            Ground.speed = 0
+            Bird.animation_time = 0
+            lost = True
+    
+        screen_sketch(screen, bird, pipes, ground, score)
+        
+if __name__ == '__main__':
+    main()
